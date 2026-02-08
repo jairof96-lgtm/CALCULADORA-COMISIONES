aae7cbd468a56696a5314dde8b2c32afe0afc7e6
jairof96-lgtm <jairof96@gmail.com>
2026-02-07 21:34:19 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
